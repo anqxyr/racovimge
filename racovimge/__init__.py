33ca203e38529a1408b7cd4e3609da8a05a87e95
anqxyr <anqxyr@gmail.com>
2017-05-23 13:01:07 +0300

    fix directory structure

---
+++ racovimge/__init__.py
@@ -0,0 +1,1 @@
+from .racovimge import random, cover
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='racovimge',
+    version='0.2',
+    description='Random Cover Image Generator',
+    long_description='',
+    url='https://github.com/anqxyr/racovimge/',
+    author='anqxyr',
+    author_email='anqxyr@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6'],
+    packages=['racovimge'],
+    package_data={'racovimge': ['templates/*']},
+    install_requires=['jinja2'],
+)
